ce59f5655054666d558180e273c547c8891425ba
kingbuzzman <buzzi.javier@gmail.com>
2019-04-23 10:57:41 +0000

    Slight change.

--- factory/django/__init__.py
+++ factory/django/__init__.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright: See the LICENSE file.
+
+"""factory_boy extensions for use with the Django framework."""
+
+from __future__ import absolute_import, unicode_literals
+
 from .base import DjangoModelFactory
 from .declarations import FileField, ImageField
 from .utils import mute_signals
