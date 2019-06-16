3d30b67551087a68e9291f0dc1c93eb37dbafdb8
kingbuzzman <buzzi.javier@gmail.com>
2019-06-16 19:42:00 +0000

    Putting this back

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'setuptools>=0.8',
     ],
     tests_require=[
-        # 'mock'
+        # 'mock',
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
