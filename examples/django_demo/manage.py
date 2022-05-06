d899c001216ae2354cc22303b268788d4904174e
kingbuzzman <buzzi.javier@gmail.com>
2022-05-06 18:10:48 -0400

    Debug

--- examples/django_demo/manage.py
+++ examples/django_demo/manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_demo.settings")
+    print(os.environ)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
